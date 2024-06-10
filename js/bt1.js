d5a8c39c04c7f0fffe7e3c9e4c931b3c65dfdf80
Admin <lmht721@gmail.com>
2024-06-10 11:47:46 +0700

    update bt1

---
+++ js/bt1.js
@@ -0,0 +1,21 @@
+inputMath = prompt("Nhap diem mon Toan");
+inputPhycis = prompt("Nhap diem mon Vat ly");
+inputChemistry = prompt("Nhap diem mon Hoa hoc");
+
+let Math = parseFloat(inputMath);
+let Phycis = parseFloat(inputPhycis);
+let Chemistry = parseFloat(inputChemistry);
+let Avarage = (Math + Phycis + Chemistry) / 3;
+let Tatol = Math + Phycis + Chemistry;
+
+if (Math > 0 && Phycis > 0 && Chemistry > 0) {
+  document.write("Diem mon Toan: " + Math + " diem");
+  document.write("</br>");
+  document.write("Diem mon Vat ly: " + Phycis + " diem");
+  document.write("</br>");
+  document.write("Diem mon Hoa hoc: " + Chemistry + " diem");
+  document.write("</br>");
+  document.write("Diem trung binh mon: " + Avarage + " diem");
+  document.write("</br>");
+  document.write("Tong: " + Tatol + " diem");
+}
